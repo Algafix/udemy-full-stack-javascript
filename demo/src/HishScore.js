56f2129c489d7d28aa9bbaf930647ca1764839fe
Aleix <algafix@gmail.com>
2020-06-10 19:37:14 +0200

    first two lessons

---
+++ demo/src/HishScore.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class HighScore extends Component {
+    
+    render() {
+        if(this.props.overTen){
+            return (
+                <h3>
+                    Beat 10 :D
+                    
+                    <button onClick={this.props.onReset}>Reset</button>
+                </h3>
+            );
+        }else{
+            return null;
+        }
+
+        
+    }
+
+}
+
+export default HighScore;
