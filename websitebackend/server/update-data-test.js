917d848ddc9920f797e8493782d5cd28c323f26e
Aleix <algafix@gmail.com>
2020-06-16 12:06:46 +0200

    Start of session 4, setup with mongoDB and robo3t to access the db outside

---
+++ websitebackend/server/update-data-test.js
@@ -0,0 +1,22 @@
+var models = require('./server.js').models;
+
+// Ways to update data
+// $ node server/update-data-test.js
+// See the results in mongo database
+models.Profile.findOrCreate({name: "nick2"}, (err, profile) => {
+    if(err) {
+        console.log("There was an error", err);
+    } else if(profile){
+        /*
+        profile.updateAttributes({
+            email: "nick@nickupdated.com"
+        }, (updateError, updated) => {
+            console.log("Saved?", updateError, updated);
+        });
+        */
+       profile.email = "nick@nickupdated2.com";
+       profile.save((ue, updated) => {
+           console.log("Updated?", updated);
+       });
+    }
+})
